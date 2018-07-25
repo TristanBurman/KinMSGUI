81ee2325b95c6011fa1c074527c1eb3aafcbe28d
TristanBurman <41572690+TristanBurman@users.noreply.github.com>
2018-07-25 13:15:32 +0100

    Add files via upload

--- GUI V0.51.py
+++ GUI V0.51.py
@@ -6,8 +6,6 @@ import pandas as pd
 import time
 from KinMS import KinMS
 
-import matplotlib.pyplot as plt
-
 
 #from IPython import get_ipython
 #ipython = get_ipython()
@@ -49,6 +47,7 @@ def AOI(cube):
     Start, Stop = int(Start), int(Stop)
     return Start, Stop   
 
+
 #Initial Boot window
 class BootWindow:
     #Initialization 
@@ -74,7 +73,7 @@ class BootWindow:
             self.master.withdraw()
             self.newWindow = TK.Toplevel(self.master)
             self.app = CustomWin(self.newWindow)
-    
+
 
 class ReadWin:
     #Initialization 
@@ -101,9 +100,9 @@ class ReadWin:
         self.SBLoaded = False
         TK.Button(SBFrame, text='Load .csv', width=10, command=lambda : self.SBLoadCSV()).grid(row=1, column=4, sticky="w")
         SBSubFrame = TK.Frame(master, bg='black', width=600, height=300)
-        SBSubFrame.grid(row=5, columnspan=4)
-        TK.Button(SBSubFrame, text='Gaussian', width=10, command=lambda : self.SBExamples(Type='Gaussian')).grid(row=2, column=0, columnspan=2, sticky="e")
-        TK.Button(SBSubFrame, text='Exponential', width=10, command=lambda : self.SBExamples(Type='Exponential')).grid(row=2, column=2, columnspan=2, sticky="w")
+        SBSubFrame.grid(row=5, columnspan=4, sticky='n')
+        TK.Button(SBSubFrame, text='Gaussian', width=10, command=lambda : self.SBExamples(Type='Gaussian')).grid(row=2, column=0, sticky="e")
+        TK.Button(SBSubFrame, text='Exponential', width=10, command=lambda : self.SBExamples(Type='Exponential')).grid(row=2, column=1, sticky="w")
     
         #Velocity profile
         TK.Label(master, text='Velocity Profile Equation', bg='black', fg='white', font='none 12 bold').grid(row=6, column=0, columnspan=4, pady=10, sticky="n")
@@ -113,6 +112,9 @@ class ReadWin:
         self.VPEqu.grid(row=1, column=0, columnspan=3, sticky='nse')
         self.VPLoaded = False
         TK.Button(VPFrame, text='Load .csv', width=10, command=lambda : self.VPLoadCSV()).grid(row=1, column=4, sticky="w")
+        VPSubFrame = TK.Frame(VPFrame, bg='black', width=600, height=300)
+        VPSubFrame.grid(row=5, columnspan=4, sticky='n')
+        TK.Button(VPSubFrame, text='Arctan', width=10, command=lambda : self.VPExamples(Type='Arctan')).grid(row=0, column=0, sticky="n")
         
         #Other parameters
         TK.Label(master, text='Other Parameters', bg='black', fg='white', font='none 12 bold').grid(row=8, column=0, columnspan=4, pady=10, sticky="n")
@@ -175,6 +177,7 @@ class ReadWin:
         FitsInputCenter = TK.Frame(FitsInputWin, bg='black')
         FitsInputCenter.grid(row=1, sticky="n")
         
+        #Section Labels
         TK.Label(FitsInputCenter, text = 'Data from .fits', bg='black', fg='white', font='none 12 bold').grid(row=0, column=0, columnspan=4, sticky='n')
         TK.Label(FitsInputCenter, text = 'Values Derived from .fits', bg='black', fg='white', font='none 12 bold').grid(row=13, column=0, columnspan=4, sticky='n')
         
@@ -189,7 +192,6 @@ class ReadWin:
         
         TK.Label(FitsInputCenter, text = 'Cell Size ("/Pix) = ', bg='black', fg='white', font='none 12 bold').grid(row=7, column=0, sticky='e')
         
-        
         TK.Label(FitsInputCenter, text = 'Number of Velocity Channels = ', bg='black', fg='white', font='none 12 bold').grid(row=8, column=0, sticky='e')
         TK.Label(FitsInputCenter, text = 'Velocity Reference Cell = ', bg='black', fg='white', font='none 12 bold').grid(row=9, column=0, sticky='e')
         TK.Label(FitsInputCenter, text = 'Velocity Cell Value (km/s) = ', bg='black', fg='white', font='none 12 bold').grid(row=10, column=0, sticky='e')
@@ -198,13 +200,10 @@ class ReadWin:
         TK.Label(FitsInputCenter, text = 'Beam Size [BMAJ, BMIN, BPA] (deg) = ', bg='black', fg='white', font='none 12 bold').grid(row=12, column=0, sticky='e')
         
         TK.Label(FitsInputCenter, text = 'Radius (") = ', bg='black', fg='white', font='none 12 bold').grid(row=14, column=0, sticky='e')
-
+        
         TK.Label(FitsInputCenter, text = 'Channel Start and Stop = ', bg='black', fg='white', font='none 12 bold').grid(row=15, column=0, sticky='e')
         TK.Label(FitsInputCenter, text = 'X axis Spatial Start and Stop = ', bg='black', fg='white', font='none 12 bold').grid(row=16, column=0, sticky='e')
         TK.Label(FitsInputCenter, text = 'Y axis Spatial Start and Stop = ', bg='black', fg='white', font='none 12 bold').grid(row=17, column=0, sticky='e')
-        
-        
-        
         
         #Creating Entry boxes
         InputXSize = TK.Entry(FitsInputCenter, width=32, bg='white')
@@ -221,10 +220,8 @@ class ReadWin:
         InputYVal.grid(row=6, column=1, columnspan=3, sticky='w')
         InputCellSize = TK.Entry(FitsInputCenter, width=32, bg='white')
         InputCellSize.grid(row=7, column=1, columnspan=3, sticky='w')
-        
         InputMaxRad = TK.Entry(FitsInputCenter, width=32, bg='white')
         InputMaxRad.grid(row=14, column=1, columnspan=3, sticky='w')
-        
         InputVelChan = TK.Entry(FitsInputCenter, width=32, bg='white')
         InputVelChan.grid(row=8, column=1, columnspan=3, sticky='w')
         InputVelRef = TK.Entry(FitsInputCenter, width=32, bg='white')
@@ -239,17 +236,14 @@ class ReadWin:
         InputBMIN.grid(row=12, column=2, sticky='w')
         InputBPA = TK.Entry(FitsInputCenter, width=10, bg='white')
         InputBPA.grid(row=12, column=3, sticky='w')
-        
         InputVStart = TK.Entry(FitsInputCenter, width=10, bg='white')
         InputVStart.grid(row=15, column=1, sticky='w')
         InputVStop = TK.Entry(FitsInputCenter, width=10, bg='white')
         InputVStop.grid(row=15, column=2, sticky='w')
-        
         InputXStart = TK.Entry(FitsInputCenter, width=10, bg='white')
         InputXStart.grid(row=16, column=1, sticky='w')
         InputXStop = TK.Entry(FitsInputCenter, width=10, bg='white')
         InputXStop.grid(row=16, column=2, sticky='w')
-        
         InputYStart = TK.Entry(FitsInputCenter, width=10, bg='white')
         InputYStart.grid(row=17, column=1, sticky='w')
         InputYStop = TK.Entry(FitsInputCenter, width=10, bg='white')
@@ -279,10 +273,9 @@ class ReadWin:
     def Loadfits(self):
         filename = askopenfilename()
         TK.Label(self.master, text=filename.split('/')[-1], bg='black', fg='white', font='none 12 bold').grid(row=1, column=2, columnspan=2, sticky="w") 
-#        self.FileInput.set(TK.Label(self.master, text=self.FileInput, bg='black', fg='white', font='none 12 bold').grid(row=0, column=1, sticky="w") )
         hdulist = fits.open(filename)
         self.scidata = hdulist[0].data.T
-        #Reading data for  header
+        #Reading data for header
         self.cellsize = abs(hdulist[0].header['CDELT2']) * 3600  #arcseconds/pixel
         self.xsize = hdulist[0].header['NAXIS1'] * self.cellsize
         self.xRef = hdulist[0].header['CRPIX1']
@@ -300,32 +293,33 @@ class ReadWin:
         except KeyError:
             BeamData = hdulist[1].data
             self.beamsize = np.array([np.mean(BeamData['BMAJ']) * 3600, np.mean(BeamData['BMIN']) * 3600, np.mean(BeamData['BPA'])])
-        try:
-            self.VelStart, self.VelStop = AOI(self.scidata.copy())
-        except NameError:
-            self.VelStart, self.VelStop = 0, 0
+        
         #Removing Stokes axis if applicable 
         if self.scidata.ndim > 3:
             self.scidata = self.scidata[:,:,:,0]
         
+        #Determin the relevant velocity channels
+        try:
+            self.VelStart, self.VelStop = AOI(self.scidata.copy())
+        except NameError:
+            self.VelStart, self.VelStop = 0, 0
+        
+        #Determin the relevant spacial area
         compressed = np.sum(self.scidata, axis=2)
         x, y = np.sum(compressed, axis=0), np.sum(compressed, axis=1)
+        Allowence = np.array([len(x)*0.05, len(y)*0.05])
         x, y = x[int(len(x)*0.1):int(len(x)*0.9)], y[int(len(y)*0.1):int(len(y)*0.9)]  #Excluding outter 10%
         xCutoff, yCutoff = 1.5 * np.mean(x), 1.5 * np.mean(y)
         xAOI, yAOI = np.where(x > xCutoff), np.where(y > yCutoff)
-        xBoundry, yBoundry = np.array([np.min(xAOI)-4, np.max(xAOI)+4]), np.array([np.min(yAOI)-4, np.max(yAOI)+4])
+        print(Allowence)
+        xBoundry, yBoundry = np.array([np.min(xAOI)-Allowence[0], np.max(xAOI)+Allowence[0]]), np.array([np.min(yAOI)-Allowence[1], np.max(yAOI)+Allowence[1]])
         if xBoundry[0] < 0: xBoundry[0] = 0
         if xBoundry[1] > len(x): xBoundry[1] = int(len(x)-1)
         if yBoundry[0] < 0: yBoundry[0] = 0
         if yBoundry[1] > len(y): yBoundry[1] = int(len(y)-1)
         self.XStart, self.XStop, self.YStart, self.YStop = int(xBoundry[0]+(len(x)*0.1)), int(xBoundry[1]+(len(x)*0.1)), int(yBoundry[0]+(len(y)*0.1)), int(yBoundry[1]+(len(y)*0.1))
         
-
-    
-        
-        
-        
-    #Load SB .csv
+    #Load SB .csv data
     def SBLoadCSV(self):
         SB_CSVName = askopenfilename()
         self.SB_CSV = pd.read_csv(SB_CSVName, delimiter=',')
@@ -333,6 +327,7 @@ class ReadWin:
         self.SBLoaded = True
         print('SB Loaded')
     
+    #Insert Example Equations
     def SBExamples(self, Type):
         """
         Fills in text box with example equations
@@ -346,7 +341,7 @@ class ReadWin:
             self.SBEqu.delete(0,TK.END)
             self.SBEqu.insert(0,Equ) 
     
-    #Load VP .csv
+    #Load VP .csv data
     def VPLoadCSV(self):
         VP_CSVName = askopenfilename()
         self.VP_CSV = pd.read_csv(VP_CSVName, delimiter=',')
@@ -354,6 +349,17 @@ class ReadWin:
         self.VPLoaded = True
         print('VP Loaded')
         
+    #Insert Example Equations
+    def VPExamples(self, Type):
+        """
+        Fills in text box with example equations
+        """
+        if Type == 'Arctan':
+            Equ='2/np.pi * [AMPLITUDE] * np.arctan(Radius)'
+            self.VPEqu.delete(0,TK.END)
+            self.VPEqu.insert(0,Equ)
+
+    #Save paramiters as .txt
     def SaveInputs(self):
         """
         Saves Paramiters file
@@ -367,13 +373,14 @@ class ReadWin:
                              '\n Disk Angle¦%s' %self.UserDiskAng.get(), \
                              '\n Inclination¦%s' %self.UserInc.get(), \
                              '\n Ratio¦%s' %self.UserRatio.get()])
-                                
         ParaData.close()
     
+    #Load paramiters .txt
     def LoadInputs(self):
         """
         Loads Paramiters file
         """
+        #Loads data
         Parafilename = askopenfilename()
         ParaData = open(Parafilename, "r" )
         ParaData = ParaData.readlines()
@@ -384,6 +391,7 @@ class ReadWin:
                   self.UserInc,\
                   self.UserRatio]
         count = 0
+        #Inputs data into entry boxes 
         for i in range(len(ParaData)):
             if bool(ParaData[i].strip()) == True:
                 inputs[count].delete(0,TK.END)
@@ -393,18 +401,24 @@ class ReadWin:
     
     #Run code
     def Run(self):
-        t0 = time.time()
-        #Read user data
+##############################################################################
+        t0 = time.time()                                                     #
+##############################################################################
+        #Read user inputs
         DiskThic, DiskAng, Inc, Ratio = float(self.UserDiskThic.get()), float(self.UserDiskAng.get()), float(self.UserInc.get()), float(self.UserRatio.get())
         #Make a copy of the data cube
         Cube = self.scidata.copy()[self.XStart:self.XStop,self.YStart:self.YStop,self.VelStart:self.VelStop]
+##############################################################################    
+        t1 = time.time()                                                     #
+        print('Get user input = ', (t1-t0))                                  #
+##############################################################################
         #Create cube mask
-        t1 = time.time()
-        print('Get user input = ', (t1-t0))
         CubeMask = GUI.smoothclip(Cube, Ratio, self.beamsize, self.cellsize, self.VelStart, self.VelStop)
+##############################################################################
+        t2 = time.time()                                                     #
+        print('Create Mask = ', (t2-t1))                                     #
+##############################################################################
         #Get SB and VP data
-        t2 = time.time()
-        print('Create Mask = ', (t2-t1))
         if self.SBLoaded == True:
             SBRadius = self.SB_Rad
             SB = self.SB_Prof
@@ -417,24 +431,27 @@ class ReadWin:
         else:
             Radius = np.arange(0, self.MaxRad, self.cellsize/2)
             VPRadius, VP = Radius, eval(self.SBEqu.get())
-        t3 = time.time()
-        print('Load SB and VP = ', t3-t2)
-        #Sim Cube
+##############################################################################
+        t3 = time.time()                                                     #
+        print('Load SB and VP = ', t3-t2)                                    #
+##############################################################################
+        #Create the Sim Cube
         ClippedXsize = len(Cube[:,0,0])*self.cellsize
         ClippedYsize = len(Cube[0,:,0])*self.cellsize
         ClippedVsize = len(Cube[0,0,:])*self.dv
-        
+            
         SimCube = KinMS(ClippedXsize, ClippedYsize, ClippedVsize, self.cellsize, self.dv, self.beamsize, Inc,\
                         sbProf=SB, sbRad=SBRadius, \
                         velProf=VP, velRad=VPRadius,\
                         posAng=DiskAng)
-        t4 = time.time()
-        print('Create Sim = ', t4-t3)
-        print('Total =', t4-t0)
-        GUI.makeplots(SimCube, ClippedXsize, ClippedYsize, ClippedVsize, self.cellsize,\
-                      self.dv, self.beamsize ,posang=DiskAng, overcube=CubeMask, pvdthick=DiskThic)
-             
-        return
+##############################################################################
+        t4 = time.time()                                                     #
+        print('Create Sim = ', t4-t3)                                        #
+        print('Total =', t4-t0)                                              #
+##############################################################################
+        
+#        GUI.makeplots(SimCube, ClippedXsize, ClippedYsize, ClippedVsize, self.cellsize,\
+#                      self.dv, self.beamsize ,posang=DiskAng, overcube=CubeMask, pvdthick=DiskThic)
 
 
 
@@ -472,7 +489,7 @@ class CustomWin:
 
 
 
-root = TK.Toplevel()
+root = TK.Toplevel()  #Change to TK.Tk() for running ouside of spyder
 my_gui = BootWindow(root)
 root.mainloop()
 
